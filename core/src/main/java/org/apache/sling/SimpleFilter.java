ae3e58e56578b582449ed70bf1f98a76af6fdf4b
Suren Konathala <konathalasuren@gmail.com>
2017-06-09 15:49:50 -0500

    first commit

---
+++ core/src/main/java/org/apache/sling/SimpleFilter.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.framework.Constants;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(
+        service = SimpleFilter.class,
+        property = {
+                Constants.SERVICE_DESCRIPTION + "=A Simple Filter",
+                Constants.SERVICE_VENDOR + "=The Apache Software Foundation",
+                "sling.filter.scope=REQUEST",
+                "service.ranking=1"
+        }
+)
+public class SimpleFilter implements Filter {
+    
+    private final Logger log = LoggerFactory.getLogger(SimpleFilter.class);
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+        log.info("filter invoked - start");
+        chain.doFilter(request, response);
+        log.info("filter invoked - end");
+    }
+
+    public void destroy() {
+    }
+
+}
